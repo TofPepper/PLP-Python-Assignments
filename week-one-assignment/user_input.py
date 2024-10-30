46da7423913e03f9d8a72433cae74e35fe1b4583
TofPepper <etofunmi3@gmail.com>
2024-10-30 20:52:11 +0100

    Initial commit

---
+++ week-one-assignment/user_input.py
@@ -0,0 +1,11 @@
+#using input function to store a variable called name
+name = input("Oh hi! I want to display a personalized message for you. Can you type in your name?   ")
+
+#using input function to ask for age and storing it in a variable called age
+age = input("How old are you " + name + "?  ")
+
+#using input function to ask user for location
+location = input("Where is your current location ?  ")
+
+#printing out the personalized message
+print(name + " what a beautiful name. Your age" +str(age) + " looks good on you and " + location + " is such a beautiful place to be "+ name +". Be sure to take pictures of yourself")
